8acce95c671c3e7eb8a2e56d83e914251ed6321b
joaojorge <joaojorgedosanjos@gmail.com>
2024-05-03 16:26:28 -0300

    :broom: cleanup: alteração do nome do arquivos para aula 02, criação aula 01

---
+++ comandos-basicos/aula02-declarando-variaveis.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char letra = 'a';
+    char numero = 0;
+    int number = 10;
+    float n = 5.25;
+    double d = 5.0e3; //5*10^3
+
+    printf( "Letra: %c\n", letra );
+    printf( "Numero: %d\n", numero );
+    printf( "Number: %d\n", number );
+    printf( "n: %f\n", n );
+    printf( "d: %f\n", d );
+
+    getchar();
+    return 0;
+}
