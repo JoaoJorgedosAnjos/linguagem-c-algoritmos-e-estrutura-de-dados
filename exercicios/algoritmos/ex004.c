0c92e8e9cefdaa1391059ef9621b6d7b8a6e78eb
joaojorge <joaojorgedosanjos@gmail.com>
2024-05-05 22:41:39 -0300

    ✨ feat: ex 004 algoritmos

---
+++ exercicios/algoritmos/ex004.c
@@ -0,0 +1,11 @@
+//Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+#include <stdio.h>
+
+int main()
+{
+    float grade1, grade2,grade3,grade4, averageGrade;
+    printf("Digite as 4 notas bimestrais:");
+    scanf("%f %f %f %f", &grade1, &grade2, &grade3, &grade4);
+    averageGrade = (grade1 + grade2 + grade3 + grade4) / 4;
+    printf("A média final é: %.2f\n", averageGrade);
+}
