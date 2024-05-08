e0e6e8404f76356abe9254757d3ac2a58c7ebf03
joaojorge <joaojorgedosanjos@gmail.com>
2024-05-08 14:08:50 -0300

    ✨ feat: aula 10 algoritmos - atribuição simplificada

---
+++ algoritmos/01-comandos-basicos/aula10-atribuicao-simplificada.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10, y = 10, z = 2;
+    x = x + z;
+    y += z; // y = y + z;
+    /*
+    y += z; y = y + z;
+    y -= z; y = y - z;
+    y *= z; y = y * z;
+    y /= z; y = y / z;
+    y %= z; y = y % z;
+    */
+    return 0;
+}
