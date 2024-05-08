3aff65a5ce74dd4c0e4ec7cef820ee29dc65514d
joaojorge <joaojorgedosanjos@gmail.com>
2024-05-08 13:50:30 -0300

    ✨ feat: aula 08 - comentários

---
+++ algoritmos/01-comandos-basicos/aula08-comentarios.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // Exemplos de operações aritméticas
+    //cometários de uma linha
+    // Variáveis do tipo int
+    int a = 10;
+    int b = 5;
+    int soma_int = a + b;
+    int subtracao_int = a - b;
+    int multiplicacao_int = a * b;
+    int divisao_int = a / b; // Divisão inteira
+    int resto_divisao_int = a % b;
+
+    //cometários de várias linhas
+    /*
+    printf("Operações com int:\n");
+    printf("Soma: %d\n", soma_int);
+    printf("Subtração: %d\n", subtracao_int);
+    printf("Multiplicação: %d\n", multiplicacao_int);
+    printf("Divisão: %d\n", divisao_int);
+    printf("Resto da divisão: %d\n", resto_divisao_int);
+    */
+
+ 
+}
